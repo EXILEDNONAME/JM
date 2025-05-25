c69a4252515ea1d7a10884788c96f58e1609519c
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2025-05-26 01:28:02 +0700

    Update #5

--- resources/views/pages/backend/__main/access-point/index.blade.php
+++ resources/views/pages/backend/__main/access-point/index.blade.php
@@ -2,19 +2,19 @@
 @section('title', 'Access Points')
 
 @section('table-header')
+<th> Status </th>
 <th> IP Address </th>
+<th> Location </th>
 <th> Device </th>
 <th> MAC Address </th>
-<th> Location </th>
 <th> Office </th>
-<th> Status </th>
 @endsection
 
 @section('table-body')
-{ data: 'col_1', 'className': 'align-middle text-nowrap', 'width': '1' },
-{ data: 'col_2', 'className': 'align-middle text-nowrap', 'width': '1' },
-{ data: 'col_3' },
-{ data: 'col_4', 'className': 'align-middle text-nowrap', 'width': '1' },
-{ data: 'col_5', 'className': 'align-middle text-nowrap', 'width': '1' },
 { data: 'status_device', 'className': 'align-middle text-nowrap', 'width': '1' },
+{ data: 'col_1', 'className': 'align-middle text-nowrap', 'width': '1' },
+{ data: 'col_2', 'className': 'align-middle text-nowrap' },
+{ data: 'col_3', 'className': 'align-middle text-nowrap', 'width': '1'  },
+{ data: 'col_4', 'className': 'align-middle text-nowrap' },
+{ data: 'col_5', 'className': 'align-middle text-nowrap text-center', 'width': '1' },
 @endsection
